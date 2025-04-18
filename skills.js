7cdff26ddbf3e1723f2c2b43de74e35d286cd274
ilariotrll <ilariotrll@gmail.com>
2025-04-18 18:11:54 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,6 @@
+function calculateNumbers(var1, var2) {
+    console.log("Calculating numbers...");
+    console.log("Variable 1: " + var1);
+    console.log("Variable 2: " + var2);
+    return var1 + var2;
+}
